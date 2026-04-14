22fd9d764a26db23ec63b32cee0ed1144ed89d41
Vlad Furov <vladfurov41@gmail.com>
2026-04-14 21:01:10 +0300

    [docs] добавлено описание усложнённого тестового задания (#4)

--- project/src/main/java/com/caselab/task/modules/echo/controller/EchoController.java
+++ project/src/main/java/com/caselab/task/modules/echo/controller/EchoController.java
@@ -3,21 +3,18 @@ package com.caselab.task.modules.echo.controller;
 
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.NonNull;
-
-
 @RestController
 @RequestMapping("/api/echo")
 public class EchoController {
+
     @GetMapping("/hello")
-    public ResponseEntity<@NonNull String> hello() {
-        return new ResponseEntity<>("Hello", HttpStatus.OK);
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity("Hello", HttpStatus.OK);
     }
 }
